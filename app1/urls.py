ab41a3d06ee13a0515cafdcc1bd816f3e0fbbb03
Alex <mathewalex806@gmail.com>
2023-11-11 17:45:15 +0530

    register page

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [ path("" , views.index, name="index"),
                 path("login", views.login_view, name= "login"),
-                path("logout", views.logout_view, name= "register"),
+                path("logout", views.logout_view, name= "logout"),
                 path("signup", views.signup, name= "signup"),
                 path("image_upload", views.image, name= "image"),
                 path("result", views.result, name= "result"),
