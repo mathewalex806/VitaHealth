e6e0d57b85877701a966434daefb69f615f71a51
Alex <mathewalex806@gmail.com>
2023-11-11 15:21:29 +0530

    login, signup, logout

--- app1/views.py
+++ app1/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse , HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
+from .models import *
+from django.db import IntegrityError
 # Create your views here.
 
 
 def  index(request):
     return render(request, 'app1/index.html')
+
+
+
 
 def login_view(request):
     if request.method == "POST":
@@ -25,6 +30,27 @@ def login_view(request):
     else:
         return render(request, "app1/login.html")
     
+
+
+    
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
+
+
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.create_user(username, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "app1/signup.html", {
+                "message": "Username already taken."
+            })
+        
+    else:
+        return render (request, "app1/signup.html")
